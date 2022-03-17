3d0d2d958c405a717ebddbab9f83240482239006
Gustavo Novais <gusttavo.developer@gmail.com>
2022-03-16 22:33:27 -0300

    Consuming github API

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,13 @@
 export function RepositoryItem(props) {
     return (
-        <li>
-            <strong>{props.repository?.name ?? 'default'}</strong>
-            <p>{props.repository.description}</p>
+        <>
+            <li>
+                <strong>{props.repository?.name}</strong>
+                <p>{props.repository.description}</p>
 
-            <a href="{props.repository.link}">Acessar repositório</a>
-        </li>
+                <a href="{props.repository.html_url}">Acessar repositório</a>
+            </li>
+            <hr />
+        </>
     );
 }
